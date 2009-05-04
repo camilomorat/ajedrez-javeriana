e4232299ffd66266499c5fb7572ed0f669f79d21
josedavid91 <josedavid91@d3a48c4c-32b5-11de-9cad-a3574e5575ef>
2009-05-04 01:48:20 +0000

    Revision 26 de abril

---
+++ trunk/ajedres.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include "programas.h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    char jugada[3];
+    int ajedrez[8][8];
+   
+    cout<<endl;         
+    cout<<"Recuerde colocar en mayusculas las letras y sin espacios\n";        
+    cout<<"Digite la jugada: \n";
+    cin>>jugada;
+         
+         //if((jugada[0]=='F')&&(jugada[1]=='I')&&(jugada[2]=='N'))
+         
+   
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
